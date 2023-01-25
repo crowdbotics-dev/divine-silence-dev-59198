17464d869ee49a26142485ee741b656b16cf6228
Crowdbotics <engineering@crowdbotics.com>
2023-01-25 00:10:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-silence-dev-59198",
+  url: "https://divine-silence-dev-59198.botics.co",
+  api: "https://divine-silence-dev-59198.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
